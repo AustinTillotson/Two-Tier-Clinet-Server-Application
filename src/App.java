8a61342e0efaf5f49839ebb0c54477138a8fcd68
AustinTillotson <austintillotson@knights.ucf.edu>
2021-06-19 13:05:07 -0400

    Added Labels to DatabaseInfoPanel

--- src/App.java
+++ src/App.java
@@ -7,6 +7,12 @@ public class App extends JFrame {
     private JPanel CommandPanel = new JPanel();
     private JPanel ConnectDBPanel = new JPanel();
     private JPanel ResultPanel = new JPanel();
+    private JLabel DatabaseInfoLabel = new JLabel("Enter Database Information");
+    private JLabel DriverLabel = new JLabel("JDBC Driver");
+    private JLabel DBLabel = new JLabel("Database URL");
+    private JLabel UsernameLabel = new JLabel("Username");
+    private JLabel PasswordLabel = new JLabel("Password");
+
 
     public App() {
         super("Project 2 - SQL Client App - (AT - CNT 4714 - Summer 2021");
@@ -19,6 +25,25 @@ public class App extends JFrame {
         DatabaseInfoPanel.setLayout(null);
         DatabaseInfoPanel.setBounds(0 , 0, 340, 200);
         //DatabaseInfoPanel.setBackground(Color.BLUE);
+        DatabaseInfoPanel.add(DatabaseInfoLabel);
+        DatabaseInfoLabel.setBounds(5, 2, 200, 15);
+        DatabaseInfoLabel.setForeground(Color.blue);
+        DatabaseInfoPanel.add(DriverLabel);
+        DriverLabel.setBounds(5, 17 , 105, 23);
+        DriverLabel.setOpaque(true);
+        DriverLabel.setBackground(Color.lightGray);
+        DatabaseInfoPanel.add(DBLabel);
+        DBLabel.setBounds(5, 42, 105, 23);
+        DBLabel.setOpaque(true);
+        DBLabel.setBackground(Color.lightGray);
+        DatabaseInfoPanel.add(UsernameLabel);
+        UsernameLabel.setBounds(5, 67, 105, 23);
+        UsernameLabel.setOpaque(true);
+        UsernameLabel.setBackground(Color.lightGray);
+        DatabaseInfoPanel.add(PasswordLabel);
+        PasswordLabel.setBounds(5, 92, 105, 23);
+        PasswordLabel.setOpaque(true);
+        PasswordLabel.setBackground(Color.lightGray);
 
         // Enter An SQL Command Panel
         add(CommandPanel);
